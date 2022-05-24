e47b79f45cbdf5aab353a6a59b191a568db37e34
williammarcotte <billmarcotte125@gmail.com>
2022-05-24 00:43:31 -0400

    Added content to generator methods

--- src/main/java/com/example/priorityorganizer/CircleGenerator.java
+++ src/main/java/com/example/priorityorganizer/CircleGenerator.java
@@ -1,23 +1,36 @@
 package com.example.priorityorganizer;
 
 import javafx.scene.Group;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class CircleGenerator extends ShapeGenerator{
 
-    public CircleGenerator(Group group){
+    Group group;
 
+    //use stackpane or borderpane for shape layout
+
+    public CircleGenerator(Group gro){
+        group = gro;
     }
 
-    public void createShape(){
 
+    public void createShape(int size){
+        Circle cir;
+
+        group.getChildren();
     }
 
     public void placeShape(){
 
     }
 
+    //see if shapes occupy the same space
     public void hasOverlap(){
+        boolean collision = true;
+        while(collision){
 
+        }
     }
 
     @Override
--- src/main/java/com/example/priorityorganizer/HelloApplication.java
+++ src/main/java/com/example/priorityorganizer/HelloApplication.java
@@ -1,17 +1,36 @@
 package com.example.priorityorganizer;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.shape.Shape;
+import java.util.ArrayDeque;
+import java.util.Random;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) throws Exception {
+
+        HBox hbox = new HBox();
+
+        Label hello = new Label("hello");
+        hbox.getChildren().add(hello);
+
+        Scene scene = new Scene(hbox, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
